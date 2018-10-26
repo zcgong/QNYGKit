0ca368378562f4dd701deb6a3e581d1526fedde6
nannanit <759010589@qq.com>
2018-10-26 23:18:55 +0800

    增加单元测试

--- QNYGKitTests/QNYGKitTests.m
+++ QNYGKitTests/QNYGKitTests.m
@@ -7,8 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "QNFlexBoxLayout.h"
+
+#define SCREEN_WIDTH    [UIScreen mainScreen].bounds.size.width
 
 @interface QNYGKitTests : XCTestCase
+
 
 @end
 
@@ -24,9 +28,81 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testStrDiv {
+    NSDictionary *attrDict = @{NSFontAttributeName:[UIFont systemFontOfSize:15]};
+    NSMutableAttributedString *mAttrString = [[NSMutableAttributedString alloc] initWithString:@"我是字符串，我是字符串，我是字符串，我是字符串，我是字符串" attributes:attrDict];
+    NSAttributedString *attrString = [mAttrString copy];
+    QNLayoutStrDiv *strDiv = [QNLayoutStrDiv layoutStrDivWithCalAttrStr:attrString];
+    [strDiv qn_layoutWithWrapContent];
+    CGRect strFrame = [attrString boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    strFrame = CGRectMake(0, 0, ceil(strFrame.size.width), ceil(strFrame.size.height));
+    XCTAssert(CGRectEqualToRect(strDiv.frame, strFrame), @"invalid");
+}
+
+- (void)testVerticalLayout {
+    QNLayoutDiv *mainDiv = [QNLayoutDiv verticalLayoutDiv];
+    QNLayoutFixedSizeDiv *fixedSizeDivA = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 50)];
+    [fixedSizeDivA qn_makeLayout:^(QNLayout *layout) {
+        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(5, 10, 12, 0));
+    }];
+    
+    QNLayoutFixedSizeDiv *fixedSizeDivB = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 50)];
+    [fixedSizeDivB qn_makeLayout:^(QNLayout *layout) {
+        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(12, 5, 8, 0));
+    }];
+    
+    [mainDiv qn_makeLayout:^(QNLayout *layout) {
+        layout.padding.equalToEdgeInsets(UIEdgeInsetsMake(3, 4, 5, 6));
+        layout.children(@[fixedSizeDivA, fixedSizeDivB]);
+    }];
+    [mainDiv qn_layoutWithSize:CGSizeMake(SCREEN_WIDTH, QNUndefinedValue)];
+    XCTAssert(CGRectEqualToRect(fixedSizeDivA.frame, CGRectMake(14, 8, 100, 50)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(fixedSizeDivB.frame, CGRectMake(9, 82, 100, 50)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(mainDiv.frame, CGRectMake(0, 0, SCREEN_WIDTH, 145)), @"div frame invalid");
+}
+
+- (void)testLinearLayout {
+    QNLayoutDiv *mainDiv = [QNLayoutDiv linerLayoutDiv];
+    QNLayoutFixedSizeDiv *fixedSizeDivA = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 50)];
+    [fixedSizeDivA qn_makeLayout:^(QNLayout *layout) {
+        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(5, 10, 12, 10));
+    }];
+    
+    QNLayoutFixedSizeDiv *fixedSizeDivB = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 60)];
+    [fixedSizeDivB qn_makeLayout:^(QNLayout *layout) {
+        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(12, 5, 8, 0));
+    }];
+    
+    [mainDiv qn_makeLayout:^(QNLayout *layout) {
+        layout.padding.equalToEdgeInsets(UIEdgeInsetsMake(3, 4, 5, 6));
+        layout.children(@[fixedSizeDivA, fixedSizeDivB]);
+    }];
+    [mainDiv qn_layoutWithSize:CGSizeMake(SCREEN_WIDTH, QNUndefinedValue)];
+    XCTAssert(CGRectEqualToRect(fixedSizeDivA.frame, CGRectMake(14, 8, 100, 50)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(fixedSizeDivB.frame, CGRectMake(129, 15, 100, 60)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(mainDiv.frame, CGRectMake(0, 0, SCREEN_WIDTH, 88)), @"div frame invalid");
+}
+
+- (void)testJustify {
+    QNLayoutDiv *mainDiv = [QNLayoutDiv linerLayoutDiv];
+    QNLayoutFixedSizeDiv *fixedSizeDivA = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 50)];
+    [fixedSizeDivA qn_makeLayout:^(QNLayout *layout) {
+        //        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(0, 10, 0, 10));
+    }];
+    
+    QNLayoutFixedSizeDiv *fixedSizeDivB = [QNLayoutFixedSizeDiv layoutFixedSizeDivWithFixedSize:CGSizeMake(100, 60)];
+    [fixedSizeDivB qn_makeLayout:^(QNLayout *layout) {
+        //        layout.margin.equalToEdgeInsets(UIEdgeInsetsMake(0, 10, 0, 0));
+    }];
+    [mainDiv qn_makeLayout:^(QNLayout *layout) {
+        layout.justifyContent.equalTo(@(QNJustifySpaceBetween));
+        layout.padding.equalToEdgeInsets(UIEdgeInsetsMake(10, 12, 15, 18));
+        layout.children(@[fixedSizeDivA, fixedSizeDivB]);
+    }];
+    [mainDiv qn_layoutWithSize:CGSizeMake(SCREEN_WIDTH, QNUndefinedValue)];
+    XCTAssert(CGRectEqualToRect(fixedSizeDivA.frame, CGRectMake(12, 10, 100, 50)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(fixedSizeDivB.frame, CGRectMake(SCREEN_WIDTH - 118, 10, 100, 60)), @"fixedSizeDivA frame invalid");
+    XCTAssert(CGRectEqualToRect(mainDiv.frame, CGRectMake(0, 0, SCREEN_WIDTH, 85)), @"div frame invalid");
 }
 
 - (void)testPerformanceExample {
